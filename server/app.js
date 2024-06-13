b118029a18d7bc286920cf4bf78b1127e1e89efe
Jason Laidley <jasonlaidley@gmail.com>
2024-06-13 15:22:17 +1000

    add people endpoint

--- server/app.js
+++ server/app.js
@@ -4,8 +4,14 @@ const app = express();
 
 const port = process.env.PORT || 4000;
 
-app.get('/', (req, res) => {
-  res.send('Home Route');
+const peopleData = [
+  { id: 1, name: 'Bruce Wayne', age: 40, location: 'Brisbane'},
+  { id: 2, name: 'Peter Parker', age: 20, location: 'Sydney'},
+  { id: 3, name: 'Clarke Kent', age: 30, location: 'Melbourne'}
+];
+
+app.get('/people', (req, res) => {
+  res.send(peopleData);
 });
 
 app.listen(port, () =>
