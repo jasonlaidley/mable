4955a72c7c8d8bd6870927865246e21019553a3c
Jason Laidley <jasonlaidley@gmail.com>
2024-06-13 19:01:23 +1000

    list items on front end

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import Button from '@mui/material/Button';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:5000/people')
+      .then(response => setData(response.data));
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {data.map(item => (
+          <div key={item.id}>
+            <p>{item.name}</p>
+            <p>{item.age}</p>
+            <p>{item.name}</p>
+          </div>
+        ))}
+        <Button variant="contained">Hello world</Button>
       </header>
     </div>
   );
--- server/app.js
+++ server/app.js
@@ -2,7 +2,13 @@ const express = require('express');
 
 const app = express();
 
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 5000;
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 const peopleData = [
   { id: 1, name: 'Bruce Wayne', age: 40, location: 'Brisbane'},
